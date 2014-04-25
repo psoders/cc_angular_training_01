0820fc07ba0418a7f2d516713d1923abf0d4890a
t.majczak <t.majczak@clearcode.cc>
2014-04-25 15:10:48 +0200

    comments view + mocks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'ngMockE2E'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -13,7 +14,38 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/comments', {
+        templateUrl: 'views/comments.html',
+        controller: 'CommentsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function($httpBackend) {
+    var comments = [
+        {
+            body: 'asd',
+            createOn: 'asd'
+        },
+        {
+            body: 'asd22',
+            createOn: 'asd22'
+        },
+        {
+            body: 'asd2233',
+            createOn: 'asd2233'
+        }
+    ];
+
+    // returns the current list of phones
+    $httpBackend.whenGET('/comments').respond(comments);
+
+    // adds a new phone to the phones array
+    $httpBackend.whenPOST('/phones').respond(function(method, url, data) {
+        var comment = angular.fromJson(data);
+        comments.push(comment);
+        return [200, comment, {}];
+    });
+    $httpBackend.whenGET(/^views\//).passThrough();
   });
