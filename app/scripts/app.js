8f59d3cefea6c430e27fcf33c07bc8403a9f6743
g.kaszuba <g.kaszuba@clearcode.cc>
2014-04-25 15:13:13 +0200

    issue crud

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,14 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/issues', {
+        templateUrl: 'views/issues.html',
+        controller: 'IssueIssuesCtrl'
+      })
+      .when('/issue/1/edit', {
+        templateUrl: 'views/issues.html',
+        controller: 'IssueIssuesCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/issue/edit.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('jiraAngularTrainingApp')
+  .controller('IssueEditCtrl', function ($scope) {
+    $scope.issue = {
+        "id": 1,
+        "title": "Add new user",
+        "description": "Lorem ipsum...",
+        "status": "In Progress",
+        "type": "Task",
+        "priority": "major",
+        "assignee": "Arek Zając",
+        "updated_at": "2013-09-12T06:20:31+0000",
+        "created_at": "2013-09-12T06:20:31+0000"
+    }
+  });
